4e1b6a50aac334265f87a66bd8229786373bed94
Alex Cline <rev.a.r.cline@gmail.com>
2021-03-28 14:10:30 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
-// Your code here
+// mappers
+
+function mapToNegativize(sourceArray) {
+  let newSourceArray = [];
+  for (const [i, v] of sourceArray.entries()) {
+    newSourceArray[i] = 0 - v;
+  }
+  return newSourceArray;
+}
+
+function mapToNoChange(sourceArray) {
+  return sourceArray;
+}
+
+function mapToDouble(sourceArray) {
+  let newSourceArray = [];
+  for (const [i, v] of sourceArray.entries()) {
+    newSourceArray[i] = v * 2;
+  }
+  return newSourceArray;
+}
+
+function mapToSquare(sourceArray) {
+  let newSourceArray = [];
+  for (const [i, v] of sourceArray.entries()) {
+    newSourceArray[i] = v * v;
+  }
+  return newSourceArray;
+}
+
+// reducers
+
+function reduceToTotal(sourceArray, startingPoint = 0) {
+  let i;
+  let out = startingPoint;
+  for (i = 0; i < sourceArray.length; i++) {
+    out += sourceArray[i];
+  }
+  return out;
+}
+
+function reduceToAllTrue(sourceArray) {
+  let out = true;
+  for (const val of sourceArray) {
+    if (!val) {
+      out = false;
+    }
+  }
+  return out;
+}
+
+function reduceToAnyTrue(sourceArray) {
+  let out = false;
+  for (const val of sourceArray) {
+    if (val) {
+      out = true;
+    }
+  }
+  return out;
+}
